6407c19f380fad52f3fabcc46953b264468315af
markEscario <mbescario@uerm.edu.ph>
2022-08-02 16:41:44 +0800

    [Fixes on getEmployeeDetails Query]

--- data/employees/index.js
+++ data/employees/index.js
@@ -14,21 +14,18 @@ const getEmployees = async () => {
 }
 
 const getSearchEmployees = async (data, res) => {
-  let lastname = data.lastname
-  let firstname = data.firstname
-  let code = data.employee_no
-  let middlename = data.middlename
-  let campus = data.campus
-  let gender = data.gender === 'MALE' ? 'M' : data.gender === 'FEMALE' ? 'F' : ''
-  let employee_department = data.employee_department
-  let employee_position = data.employee_position
-  let employee_status = data.employee_status
-  let employee_class = data.employee_class
-  let isActive = data.isActive
-
   try {
-    let pool = await sql.connect(config.sql);
-    let request = new sql.Request(pool);
+    let lastname = data.lastname
+    let firstname = data.firstname
+    let code = data.employee_no
+    let middlename = data.middlename
+    let campus = data.campus
+    let gender = data.gender === 'MALE' ? 'M' : data.gender === 'FEMALE' ? 'F' : ''
+    let employee_department = data.employee_department
+    let employee_position = data.employee_position
+    let employee_status = data.employee_status
+    let employee_class = data.employee_class
+    let isActive = data.isActive
     lastname = `%${lastname}%`;
     firstname = `%${firstname}%`;
     middlename = `%${middlename}%`;
@@ -40,20 +37,57 @@ const getSearchEmployees = async (data, res) => {
     employee_status = `%${employee_status}%`;
     employee_class = `%${employee_class}%`;
     isActive = `%${isActive}%`;
+    let pool = await sql.connect(config.sql);
+    let request = new sql.Request(pool);
 
-    const empResult = await request.query`SELECT * FROM [UE database]..[vw_Employees] INNER JOIN 
-                                          [UE database]..Education ON vw_Employees.CODE = Education.EmployeeCode 
-                                          WHERE LASTNAME LIKE ${lastname} AND 
-                                          FIRSTNAME LIKE ${firstname} AND 
-                                          MIDDLENAME LIKE ${middlename} AND 
-                                          CODE LIKE ${code} AND 
-                                          [CAMPUS CODE] LIKE ${campus} AND 
-                                          GENDER LIKE ${gender} AND
-                                          DEPT_DESC LIKE ${employee_department} AND
-                                          POS_DESC LIKE ${employee_position} AND
-                                          EMP_CLASS_DESC LIKE ${employee_class} AND
-                                          EMP_STATUS_DESC LIKE ${employee_status} AND
-                                          IS_ACTIVE = '1' ORDER BY LASTNAME DESC`;
+    const empResult = await request.query`SELECT 
+                                            Emp.CODE,
+                                            Emp.FIRSTNAME,
+                                            Emp.LASTNAME,
+                                            Emp.MIDDLENAME,
+                                            Emp.EXTNAME,
+                                            Emp.NAME,
+                                            Emp.[CAMPUS CODE],
+                                            Emp.GENDER,
+                                            Emp.DEPT_DESC,
+                                            Emp.POS_DESC,
+                                            Emp.EMP_CLASS_DESC,
+                                            Emp.EMP_STATUS_DESC,
+                                            Emp.IS_ACTIVE,
+                                            Emp.EMAIL,
+                                            Ed.EmployeeCode,
+                                            Emp.EMP_CLASS_CODE
+                                            FROM [UE database]..[vw_Employees] AS Emp INNER JOIN 
+                                            [UE database]..Education AS Ed ON Emp.CODE = Ed.EmployeeCode 
+                                            WHERE Emp.LASTNAME LIKE ${lastname} AND 
+                                            Emp.FIRSTNAME LIKE ${firstname} AND 
+                                            Emp.MIDDLENAME LIKE ${middlename} AND 
+                                            Emp.CODE LIKE ${code} AND 
+                                            Emp.[CAMPUS CODE] LIKE ${campus} AND 
+                                            Emp.GENDER LIKE ${gender} AND
+                                            Emp.DEPT_DESC LIKE ${employee_department} AND
+                                            Emp.POS_DESC LIKE ${employee_position} AND
+                                            Emp.EMP_CLASS_DESC LIKE ${employee_class} AND
+                                            Emp.EMP_STATUS_DESC LIKE ${employee_status} AND
+                                            Emp.IS_ACTIVE = '1' 
+                                            GROUP BY 
+                                            Emp.CODE,
+                                            Emp.FIRSTNAME,
+                                            Emp.LASTNAME,
+                                            Emp.MIDDLENAME,
+                                            Emp.EXTNAME,
+                                            Emp.NAME,
+                                            Emp.[CAMPUS CODE],
+                                            Emp.GENDER,
+                                            Emp.DEPT_DESC,
+                                            Emp.POS_DESC,
+                                            Emp.EMP_CLASS_DESC,
+                                            Emp.EMP_STATUS_DESC,
+                                            Emp.IS_ACTIVE,
+                                            Emp.EMAIL,
+                                            Ed.EmployeeCode,
+                                            Emp.EMP_CLASS_CODE 
+                                            ORDER BY Emp.LASTNAME DESC`;
     console.log('res: '.empResult)
     return empResult.recordset
 
@@ -62,19 +96,18 @@ const getSearchEmployees = async (data, res) => {
   }
 }
 const getSearchEmployeeDetails = async (data, res) => {
-  let lastname = data.lastname
-  let firstname = data.firstname
-  let code = data.employee_no
-  let middlename = data.middlename
-  let campus = data.campus
-  let gender = data.gender === 'MALE' ? 'M' : data.gender === 'FEMALE' ? 'F' : ''
-  let employee_department = data.employee_department
-  let employee_position = data.employee_position
-  let employee_status = data.employee_status
-  let employee_class = data.employee_class
-  let isActive = data.isActive
-
   try {
+    let lastname = data.lastname
+    let firstname = data.firstname
+    let code = data.employee_no
+    let middlename = data.middlename
+    let campus = data.campus
+    let gender = data.gender === 'MALE' ? 'M' : data.gender === 'FEMALE' ? 'F' : ''
+    let employee_department = data.employee_department
+    let employee_position = data.employee_position
+    let employee_status = data.employee_status
+    let employee_class = data.employee_class
+    let isActive = data.isActive
     lastname = `%${lastname}%`;
     firstname = `%${firstname}%`;
     middlename = `%${middlename}%`;
@@ -89,19 +122,76 @@ const getSearchEmployeeDetails = async (data, res) => {
     let pool = await sql.connect(config.sql);
     let request = new sql.Request(pool);
 
-    const empDetails = await request.query`SELECT * FROM [UE database]..[vw_Employees] INNER JOIN 
-                                         [UE database]..Education ON vw_Employees.CODE = Education.EmployeeCode 
-                                          WHERE LASTNAME LIKE ${lastname} AND 
-                                          FIRSTNAME LIKE ${firstname} AND 
-                                          MIDDLENAME LIKE ${middlename} AND 
-                                          CODE LIKE ${code} AND 
-                                          [CAMPUS CODE] LIKE ${campus} AND 
-                                          GENDER LIKE ${gender} AND
-                                          DEPT_DESC LIKE ${employee_department} AND
-                                          POS_DESC LIKE ${employee_position} AND
-                                          EMP_CLASS_DESC LIKE ${employee_class} AND
-                                          EMP_STATUS_DESC LIKE ${employee_status} AND
-                                          IS_ACTIVE = '1' ORDER BY LASTNAME DESC`;
+    const empDetails = await request.query`SELECT 
+                                            Emp.CODE,
+                                            Emp.FIRSTNAME,
+                                            Emp.LASTNAME,
+                                            Emp.MIDDLENAME,
+                                            Emp.EXTNAME,
+                                            Emp.NAME,
+                                            Emp.[CAMPUS CODE],
+                                            Emp.GENDER,
+                                            Emp.DEPT_DESC,
+                                            Emp.POS_DESC,
+                                            Emp.EMP_CLASS_DESC,
+                                            Emp.EMP_STATUS_DESC,
+                                            Emp.IS_ACTIVE,
+                                            Emp.EMAIL,
+                                            Ed.EmployeeCode,
+                                            Emp.EMP_CLASS_CODE,
+                                            Emp.AGE,
+                                            Emp.BDATE,
+                                            Emp.ADDRESS,
+                                            Emp.CONTACT_ADDRESS,
+                                            Emp.MOBILENO,
+                                            Emp.HIRED,
+                                            Emp.REGULARIZED,
+                                            Emp.RESIGNED,
+                                            Emp.[SERVICE YEARS],
+                                            Emp.TIN,
+                                            EMP.CIVIL_STATUS_DESC
+                                            FROM [UE database]..[vw_Employees] AS Emp INNER JOIN 
+                                            [UE database]..Education AS Ed ON Emp.CODE = Ed.EmployeeCode 
+                                            WHERE Emp.LASTNAME LIKE ${lastname} AND 
+                                            Emp.FIRSTNAME LIKE ${firstname} AND 
+                                            Emp.MIDDLENAME LIKE ${middlename} AND 
+                                            Emp.CODE LIKE ${code} AND 
+                                            Emp.[CAMPUS CODE] LIKE ${campus} AND 
+                                            Emp.GENDER LIKE ${gender} AND
+                                            Emp.DEPT_DESC LIKE ${employee_department} AND
+                                            Emp.POS_DESC LIKE ${employee_position} AND
+                                            Emp.EMP_CLASS_DESC LIKE ${employee_class} AND
+                                            Emp.EMP_STATUS_DESC LIKE ${employee_status} AND
+                                            Emp.IS_ACTIVE = '1' 
+                                            GROUP BY 
+                                            Emp.CODE,
+                                            Emp.FIRSTNAME,
+                                            Emp.LASTNAME,
+                                            Emp.MIDDLENAME,
+                                            Emp.EXTNAME,
+                                            Emp.NAME,
+                                            Emp.[CAMPUS CODE],
+                                            Emp.GENDER,
+                                            Emp.DEPT_DESC,
+                                            Emp.POS_DESC,
+                                            Emp.EMP_CLASS_DESC,
+                                            Emp.EMP_STATUS_DESC,
+                                            Emp.IS_ACTIVE,
+                                            Emp.EMAIL,
+                                            Ed.EmployeeCode,
+                                            Emp.EMP_CLASS_CODE,
+                                            Emp.AGE,
+                                            Emp.BDATE,
+                                            Emp.ADDRESS,
+                                            Emp.CONTACT_ADDRESS,
+                                            Emp.MOBILENO,
+                                            Emp.HIRED,
+                                            Emp.REGULARIZED,
+                                            Emp.RESIGNED,
+                                            Emp.[SERVICE YEARS],
+                                            Emp.TIN,
+                                            EMP.CIVIL_STATUS_DESC
+                                            ORDER BY Emp.LASTNAME DESC`;
     console.log('res: '.empDetails)
     return empDetails.recordset
 
